deacc25421fba6166b7a581d51e7e3b86a7f6ce2
Jake Tepper <Jake@Jakes-MacBook-Pro.local>
2017-03-27 16:40:11 -0400

    Adding Java program but not sure which folder is my project..

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+/**
+ * Created by Jake on 3/27/17.
+ * This is my attempt at printing my name.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        /*I'm not sure what the above line does */
+        System.out.println("Jake Tepper");
+        System.out.println(4*2);
+        int MathTest = 44*233;
+        //Creates a variable and is like the old 'var' command
+        System.out.println(MathTest);
+        //Prints the value of my variable
+        int MathTest2 = MathTest%3;
+        //Shows me the remaining ballance after 3 can not be divided anymore
+        /*Now I want to print my new variable below and I want to
+        Use multiple comment lines as I did in the start.
+         */
+        System.out.println(MathTest2);
+        //Now I will test my code by creating a test.
+        if (MathTest2 == 1) {
+            System.out.println("All Tests Passed!");
+        }
+        else {
+            System.out.println("Why you do this /( says Yamir /)");
+        //I understand the if statements but I don't get why I need more of the /{ / }
+        }
+
+
+
+    }
+
+}
